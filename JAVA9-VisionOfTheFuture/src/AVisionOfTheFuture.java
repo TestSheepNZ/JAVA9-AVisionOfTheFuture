1a515a0d00a718a894351259645754e97b4d562b
TestSheepNZ <mike_talks@hotmail.com>
2016-08-31 21:05:47 +1200

    Done

---
+++ JAVA9-VisionOfTheFuture/src/AVisionOfTheFuture.java
@@ -0,0 +1,40 @@
+/*
+ * This program is part of my learning Java series
+ * This one looks at different forms of loops
+ * 
+ * Find the blog here,
+ * http://testsheepnz.blogspot.co.nz/2016/08/java-7-conditional-world-of-ifs.html
+ * 
+ * YOU ALREADY KNOW WHAT WAITS FOR YOU IN ROOM 101
+ * 
+ * For more information - please reread.
+ * 
+ * Mike Talks, Aug 2016
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+ 
+public class AVisionOfTheFuture {
+ 
+       /*
+       * "If you want a vision of the future,
+        * imagine a boot stamping on a human face - forever"
+       * George Orwell, 1984
+       */
+      
+       public void imagineABoot()
+       {
+              System.out.println("Stamping on a human face!");
+       }
+      
+       @Test
+       public void forVisionOfTheFuture()
+       {
+              for (int counter = 1 ; counter > 0 ; counter++)
+              {
+                     imagineABoot();
+              }
+       }
+      
+}
